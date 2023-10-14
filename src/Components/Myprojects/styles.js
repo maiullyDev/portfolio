e00f9b64f2a1a647499c63c5f1acead15d32a4d6
maiullyDev <maiully.dev@gmail.com>
2023-10-14 01:31:39 -0300

    feat: Make the Myprojects component responsive

--- src/Components/Myprojects/styles.js
+++ src/Components/Myprojects/styles.js
@@ -5,7 +5,11 @@ export const MyProjectsContainer = styled.section`
   flex-direction: column;
   align-items: center;
 
-  margin: 16rem auto;
+  margin: 16rem 3rem;
+
+  @media (max-width: 975px) {
+    margin: 10rem 3rem;
+  }
 `;
 
 export const TitleSection = styled.div`
@@ -20,11 +24,23 @@ export const TitleSection = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 30px;
+
+  @media (max-width: 975px) {
+    font-size: 20px;
+  }
 `;
 
 export const ProjectsContainer = styled.div`
   display: flex;
   gap: 1.5rem;
+  flex-wrap: wrap;
+
+  @media (max-width: 975px) {
+    max-width: 505px;
+  }
+  @media (max-width: 620px) {
+    max-width: 240px;
+  }
 `;
 
 export const ProjectContainer = styled.div`
@@ -45,11 +61,37 @@ export const ProjectContainer = styled.div`
     transform: scale(1.1);
     background-color: rgba(181, 249, 253, 0.42);
   }
+
+  @media (max-width: 1300px) {
+    width: 20rem;
+    height: 15rem;
+
+    border-radius: 1.3rem;
+  }
+  @media (max-width: 1150px) {
+    width: 18rem;
+    height: 13rem;
+  }
+  @media (max-width: 1050px) {
+    width: 17rem;
+    height: 12rem;
+  }
+  @media (max-width: 975px) {
+    width: 15rem;
+    height: 10rem;
+  }
 `;
 
 export const WebsiteImage = styled.img`
   height: 60%;
   margin-top: 30px;
+
+  @media (max-width: 1300px) {
+    height: 55%;
+  }
+  @media (max-width: 975px) {
+    margin-top: 20px;
+  }
 `;
 
 export const TitleProject = styled.p`
@@ -69,4 +111,20 @@ export const TitleProject = styled.p`
 
   position: absolute;
   bottom: 0px;
+
+  @media (max-width: 1300px) {
+    width: 20rem;
+    border-radius: 0rem 0rem 1.3rem 1.3rem;
+  }
+  @media (max-width: 1150px) {
+    width: 18rem;
+  }
+  @media (max-width: 1050px) {
+    width: 17rem;
+  }
+  @media (max-width: 975px) {
+    width: 15rem;
+    padding-top: 0.4rem;
+    padding-bottom: 0.4rem;
+  }
 `;
